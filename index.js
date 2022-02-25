d069e670c8eaaf6142a995083ae64c81b1fab3c8
Sher-Kon <sher-kon@ya.ru>
2022-02-25 09:24:42 +0300

    настроил npm run start/dev

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-//console.log('In Node We Trust');
-
 const express = require('express');
-// Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
+const router = require('./router.js'); // импортируем роутер
 
+const { PORT = 3000 } = process.env;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(
-        `<html>
-        <body>
-            <p>Ответ на сигнал из далёкого космоса</p>
-        </body>
-        </html>`
-    );
-}); 
+app.use('/', router); // запускаем
 
 app.listen(PORT, () => {
-    // Если всё работает, консоль покажет, какой порт приложение слушает
-    console.log(`App listening on port ${PORT}`)
-}) 
+    console.log(`App listening on port ${PORT}`);
+});
