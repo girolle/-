9289e5f1093c55ff8c3ffad29a904de036ae503b
Шерстнев Константин Адольфович <Sher@s-tech.ru>
2022-02-26 18:11:50 +0300

    сервер подключил к БД

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const router = require('./router.js'); // импортируем роутер
+const router = require('./routes/user.js'); // импортируем роутер
 const mongoose = require('mongoose');
 const path = require('path');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-/*
+
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mydb', {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+//  useCreateIndex: true,
+//  useFindAndModify: false
 });
-*/
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router); // запускаем
 
