760a70611da7de34542ba2da5c7c54e0a8a7a1b8
Sher-Kon <sher-kon@ya.ru>
2022-03-01 11:14:22 +0300

    ставит снимает лайки

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,7 +4,7 @@
 const Card = require('../models/card');
 
 module.exports.createCard = (req, res) => {
-  console.log("owner: "+req.user._id); // _id станет доступен
+  console.log("owner: " + req.user._id); // _id станет доступен
   const owner = req.user._id;
   const { name, link } = req.body;
   const likes = [];
@@ -25,4 +25,26 @@ module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id) //
     .then(card => res.send({ card: card }))//
     .catch(() => res.status(500).send({ message: 'Произошла ошибка deleteCard' }));
+};
+
+
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+  .then(card => res.send({ card: card }))//
+  .catch(() => res.status(500).send({ message: 'Произошла ошибка likeCard' }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+  .then(card => res.send({ card: card }))//
+  .catch(() => res.status(500).send({ message: 'Произошла ошибка dislikeCard' }));
 };
--- routes/cards.js
+++ routes/cards.js
@@ -2,10 +2,12 @@
 // это файл маршрутов
 
 const routerCards = require('express').Router(); // создали роутер
-const { createCard, getCards, deleteCard } = require('../controllers/cards');
+const { createCard, getCards, deleteCard, likeCard, dislikeCard } = require('../controllers/cards');
 
 routerCards.get('/cards', getCards);
 routerCards.post('/cards', createCard);
 routerCards.delete('/cards/:id', deleteCard);
+routerCards.put('/cards/:cardId/likes', likeCard);
+routerCards.delete('/cards/:cardId/likes', dislikeCard);
 
 module.exports = routerCards; // экспортировали роутер
